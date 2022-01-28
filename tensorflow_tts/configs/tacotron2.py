48087bc2764a0f65673c833a1f1ee500db2b9927
Bemnet Merha <bemnet.merha@databricks.com>
2022-01-27 22:57:13 -0800

    amharic

--- tensorflow_tts/configs/tacotron2.py
+++ tensorflow_tts/configs/tacotron2.py
@@ -22,7 +22,7 @@ from tensorflow_tts.processor.baker import BAKER_SYMBOLS as bk_symbols
 from tensorflow_tts.processor.libritts import LIBRITTS_SYMBOLS as lbri_symbols
 from tensorflow_tts.processor.ljspeechu import LJSPEECH_U_SYMBOLS as lju_symbols
 from tensorflow_tts.processor.synpaflex import SYNPAFLEX_SYMBOLS as synpaflex_symbols
-
+from tensorflow_tts.processor.amharic import AMHARIC_SYMBOLS as amharic_symbols
 
 class Tacotron2Config(BaseConfig):
     """Initialize Tacotron-2 Config."""
@@ -72,6 +72,8 @@ class Tacotron2Config(BaseConfig):
             self.vocab_size = len(lju_symbols)
         elif dataset == "synpaflex":
             self.vocab_size = len(synpaflex_symbols)
+        elif dataset == "amharic":
+            self.vocab_size = len(amharic_symbols)        
         else:
             raise ValueError("No such dataset: {}".format(dataset))
         self.embedding_hidden_size = embedding_hidden_size
